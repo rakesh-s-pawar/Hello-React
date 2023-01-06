57ac165dca93857f40bcdadb4492e1a68a013234
rakesh-s-pawar <119561448+rakesh-s-pawar@users.noreply.github.com>
2023-01-06 23:58:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,28 +7,12 @@ const heading = React.createElement(
     id: "title",
     key: "h1",
   },
-  "Heading 1"
+  "Namaste React"
 );
 
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "Heading 2"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
-);
-console.log(heading);
+const heading2 = (<h1 id="title" key="h1">Namaste React</h1>);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Passing a react element inside the root
-root.render(container);
+root.render(heading);
