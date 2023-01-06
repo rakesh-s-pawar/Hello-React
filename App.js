de90493130881abc159ff6246eff2338c078eacb
rakesh-s-pawar <119561448+rakesh-s-pawar@users.noreply.github.com>
2023-01-07 01:43:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "Namaste React"
+// This is known as a React Element
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
 );
 
-const heading2 = (<h1 id="title" key="h1">Namaste React</h1>);
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1>This is functional component</h1>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Passing a react element inside the root
-root.render(heading);
+root.render(<HeaderComponent />);
