bd2e212a3b9de4cc6e0b7fa5f6e418039c59d82f
rakesh-s-pawar <rakeshpawar08@gmail.com>
2023-01-15 15:54:39 +0530

    Chapter_05_Lets_get_Hooked

---
+++ src/components/Body.js
@@ -0,0 +1,51 @@
+import { restaurantList } from "../constants";
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return filterData;
+}
+
+const Body = () => {
+  // searchText is a local state variable
+  const [searchText, setSearchText] = useState(""); // Default value as KFC
+
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
